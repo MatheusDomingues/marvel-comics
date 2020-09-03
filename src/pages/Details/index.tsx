c9c4558462ed827c50d5690ad2acb38b743c9e12
MatheusDomingues <matheus_luige@hotmail.com>
2020-09-03 18:17:09 -0300

    footer reformed

---
+++ src/pages/Details/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Details() {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Details;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Comics from './pages/Comics';
+import Reader from './pages/Reader';
+import Details from './pages/Details';
 
 function Routes() {
   return(
     <BrowserRouter>
       <Route path="/" exact component={Home} />
       <Route path="/comics" component={Comics} />
+      <Route path="/reader" component={Reader} />
+      <Route path="/details" component={Details} />
     </BrowserRouter>
   );
 };
