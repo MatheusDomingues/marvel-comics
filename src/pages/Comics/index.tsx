635df85064b01f5943ed31dd79ffd838b284f00a
MatheusDomingues <matheus_luige@hotmail.com>
2020-09-06 16:53:24 -0300

    carousel complete

--- src/pages/Comics/index.tsx
+++ src/pages/Comics/index.tsx
@@ -1,41 +1,195 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import api from '../../services/api';
 
-import PageHeader from '../../components/PageHeader';
 import Footer from '../../components/Footer';
-import Heroes, { Heroe } from '../../components/Heroes';
 
+import comicImage from '../../assets/images/comic.jpg';
+
+import Slider from 'react-slick';
 import './styles.css';
-export default class Comics extends React.Component {
-  state: any = {
-    data: null,
-  };
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+interface HeroProps {
+  id: number;
+  name: string;
+  comic: [];
+  resourceURI: string;
+  description: string;
+};
 
-  async componentDidMount() {
+const Comics: React.FC<HeroProps> = () => {
+  const [heroes, setHeroes] = useState([]);
+  const [comics, setComics] = useState([]);
+
+  useEffect(() => {
+    async function loadHeroes() {
+      const res = await api.get('');
+ 
+      setHeroes(res.data.data.results);
+    };
+
+    loadHeroes();
+
+  }, []);
+
+  async function loadComics() {
     const res = await api.get('');
 
-    this.setState({ data: res.data.data.results });
+    for (let hero in heroes) {
+      setComics(res.data.data.results[hero].comic);
+    };
   };
-  
-  render() {
-    const [heroes, setHeroes] = useState([]);
-    setHeroes(this.state.data);
 
-    console.log(heroes);
-    
-    return (
-      <div id="page-comics">
-        <PageHeader />
+  loadComics();
 
-        <main>
-          {heroes.map((heroe: Heroe) => {
-            return <Heroes key={heroe.id} heroe={heroe} />;
-          })}
-        </main>
-
-        <Footer />
-      </div>
-    );
+  let settings = {
+    infinite: false,
+    dots: false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
+
+  return (
+    <div id="page-comics">
+      <main>
+
+        <article className="hero-item">
+          <div className="hero-name">
+            <h2>Mystery Men</h2>
+          </div>
+          <Slider {...settings}>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+            
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+            
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+            
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+            
+          </Slider>
+        </article>
+
+        <article className="hero-item">
+          <div className="hero-name">
+            <h2>3D-Man</h2>
+          </div>
+          <Slider {...settings}>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+            
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+            
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+            
+            <div className="comic-card">
+              <img src={comicImage} alt="Comic-name"/>
+              <p>Avengers: The Beginner</p>
+            </div>
+            
+          </Slider>
+        </article>
+
+      </main>
+
+      <Footer />
+    </div>
+  );
 };
+
+export default Comics;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,16 +3,12 @@ import { BrowserRouter, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Comics from './pages/Comics';
-import Read from './pages/Read';
-import Details from './pages/Details';
 
 function Routes() {
   return(
     <BrowserRouter>
       <Route path="/" exact component={Home} />
       <Route path="/comics" component={Comics} />
-      <Route path="/read" component={Read} />
-      <Route path="/details" component={Details} />
     </BrowserRouter>
   );
 };
